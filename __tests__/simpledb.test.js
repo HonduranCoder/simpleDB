5bb3516396958c0ac14f1753597f1824d530ce47
Estefani <stef45689@gmail.com>
2021-10-28 12:16:39 -0500

    save function and test passing

--- __tests__/simpledb.test.js
+++ __tests__/simpledb.test.js
@@ -1,29 +1,30 @@
 const { mkdir, rm } = require('fs/promises');
-const SimpleDB = require('../simpledb.js');
+const SimpleDB = require('../simpledb/simpledb.js');
 
-describe('SimpleDB', ()=>{
-    const rootDir = '../simpledb/simpledb.js';
+describe('SimpleDB', () => {
+  const rootDir = '../simpledb/newobject';
 
-    beforeEach(() => {
-        return rm(rootDir, { force: true, recursive: true }).then(() =>
-          mkdir(rootDir, { recursive: true })
-        );
-      });
-
-      it('should assign an id property to the object', ()=> {
-          const firstDb= new SimpleDB(rootDir); 
-          const newFile = {
-            name: 'new',
-            text: 'random'
-          };
-          
-          
-          //expectation (file after modification of save function)
-
-          return firstDb
-          .save(newFile)
-          //.save function 
-          //.get by id function , result in .then
-          //toEqual JSON object
-          .then(() => expect(whateverID).toEqual(whateverID));
+  beforeEach(() => {
+    return rm(rootDir, { force: true, recursive: true }).then(() =>
+      mkdir(rootDir, { recursive: true })
+    );
   });
+
+  it('should assign an id property to the object', () => {
+    const firstDb = new SimpleDB(rootDir);
+    const newObject = {
+      name: 'new',
+      text: 'random',
+    }; //
+    //expectation (file after modification of save function)
+
+    return (
+      firstDb
+        .save(newObject)
+        //.save function
+        //.get by id function , result in .then
+        //toEqual JSON object
+        .then(() => expect(newObject.id).toEqual(expect.any(String)))
+    );
+  });
+});
--- simpledb/simpledb.js
+++ simpledb/simpledb.js
@@ -1,5 +1,26 @@
-class SimpleDB {
-  constructor() {}
-}
+const { writeFile, readFile } = require('fs/promises');
+const path = require('path');
+const shortid = require('shortid');
 
+class SimpleDB {
+  constructor(rootDir) {
+    const newFile = `${shortid.generate()}.json`;
+    this.newFolder = path.join(rootDir, newFile);
+  }
+
+  //save
+  save(newObject) {
+    //add the id to the new object
+    console.log(newObject);
+    const newId = shortid.generate();
+    newObject.id = newId;
+    console.log(newObject);
+    //stringify the new object (stringy object instead of newObject)
+    const stringyObj = JSON.stringify(newObject);
+    //const readIt = readFile(this.newFolder, newId);
+    return writeFile(this.newFolder, stringyObj);
+  }
+}
+//write file for saving
+//read file for file (aynchronous functions), add an id
 module.exports = SimpleDB;
